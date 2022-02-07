ab44f8e9b81f0f76cd96960028dc57eed8438c76
Chloe Thies <chloe.r.thies@gmail.com>
2022-02-07 12:27:35 -0800

    front end set up

--- src/App.js
+++ src/App.js
@@ -7,93 +7,93 @@ import "@aws-amplify/ui-react/styles.css";
 
 import { listNotes } from "./graphql/queries";
 import {
-	createNote as createNoteMutation,
-	deleteNote as deleteNoteMutation,
+  createNote as createNoteMutation,
+  deleteNote as deleteNoteMutation,
 } from "./graphql/mutations";
 
 const initialFormState = { name: "", description: "" };
 
 function loadScreen(signOut, user) {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>Hey {user.username}, welcome to Quizzards!</p>
-				<button onClick={signOut}>Sign out</button>
-			</header>
-		</div>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hey {user.username}, welcome to Quizzards!</p>
+        <button onClick={signOut}>Sign out</button>
+      </header>
+    </div>
+  );
 }
 
 function NotesPage(signOut, user) {
-	const [notes, setNotes] = useState([]);
-	const [formData, setFormData] = useState(initialFormState);
+  const [notes, setNotes] = useState([]);
+  const [formData, setFormData] = useState(initialFormState);
 
-	useEffect(() => {
-		fetchNotes();
-	}, []);
+  useEffect(() => {
+    fetchNotes();
+  }, []);
 
-	async function fetchNotes() {
-		const apiData = await API.graphql({ query: listNotes });
-		setNotes(apiData.data.listNotes.items);
-	}
+  async function fetchNotes() {
+    const apiData = await API.graphql({ query: listNotes });
+    setNotes(apiData.data.listNotes.items);
+  }
 
-	async function createNote() {
-		if (!formData.name || !formData.description) return;
-		await API.graphql({
-			query: createNoteMutation,
-			variables: { input: formData },
-		});
-		setNotes([...notes, formData]);
-		setFormData(initialFormState);
-	}
+  async function createNote() {
+    if (!formData.name || !formData.description) return;
+    await API.graphql({
+      query: createNoteMutation,
+      variables: { input: formData },
+    });
+    setNotes([...notes, formData]);
+    setFormData(initialFormState);
+  }
 
-	async function deleteNote({ id }) {
-		const newNotesArray = notes.filter((note) => note.id !== id);
-		setNotes(newNotesArray);
-		await API.graphql({
-			query: deleteNoteMutation,
-			variables: { input: { id } },
-		});
-	}
+  async function deleteNote({ id }) {
+    const newNotesArray = notes.filter((note) => note.id !== id);
+    setNotes(newNotesArray);
+    await API.graphql({
+      query: deleteNoteMutation,
+      variables: { input: { id } },
+    });
+  }
 
-	return (
-		<div className="App">
-			<h1>My Notes App</h1>
-			<p>Hey {user.username}, welcome to Quizzards!</p>
-			<button onClick={signOut}>Sign out</button>
-			<input
-				onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-				placeholder="Note name"
-				value={formData.name}
-			/>
-			<input
-				onChange={(e) =>
-					setFormData({ ...formData, description: e.target.value })
-				}
-				placeholder="Note description"
-				value={formData.description}
-			/>
-			<button onClick={createNote}>Create Note</button>
-			<div style={{ marginBottom: 30 }}>
-				{notes.map((note) => (
-					<div key={note.id || note.name}>
-						<h2>{note.name}</h2>
-						<p>{note.description}</p>
-						<button onClick={() => deleteNote(note)}>Delete note</button>
-					</div>
-				))}
-			</div>
-		</div>
-	);
+  return (
+    <div className="App">
+      <h1>My Notes App</h1>
+      <p>Hey {user.username}, welcome to Quizzards!</p>
+      <button onClick={signOut}>Sign out</button>
+      <input
+        onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+        placeholder="Note name"
+        value={formData.name}
+      />
+      <input
+        onChange={(e) =>
+          setFormData({ ...formData, description: e.target.value })
+        }
+        placeholder="Note description"
+        value={formData.description}
+      />
+      <button onClick={createNote}>Create Note</button>
+      <div style={{ marginBottom: 30 }}>
+        {notes.map((note) => (
+          <div key={note.id || note.name}>
+            <h2>{note.name}</h2>
+            <p>{note.description}</p>
+            <button onClick={() => deleteNote(note)}>Delete note</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 function App() {
-	return (
-		<Authenticator>
-			{({ signOut, user }) => NotesPage(signOut, user)}
-		</Authenticator>
-	);
+  return (
+    <Authenticator>
+      {({ signOut, user }) => NotesPage(signOut, user)}
+    </Authenticator>
+  );
 }
 
 export default withAuthenticator(App);
--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+//import App from "./App2";
+import App from "./App2";
 import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.css";
 
 import Amplify from "aws-amplify";
 import config from "./aws-exports";
 Amplify.configure(config);
 
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById("root")
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
